804edd9080d257ce05e71aa597664aff5b791af0
ddrdi459-png <ddrdi459@gmail.com>
2026-08-17 22:26:34 +0300

    Add Supabase public client configuration

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+window.TSARANORO_SUPABASE_URL = 'https://ksdhdceasgubtixyzqdd.supabase.co';
+window.TSARANORO_SUPABASE_KEY = 'sb_publishable_i_lzT1JrzRG2iFmnTtU9pQ_EJKCzXe5';
+window.TSARANORO_ADMIN_UID = '1717cd49-05c9-4321-8007-d4d317ddc630';
