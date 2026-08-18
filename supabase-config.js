4a1064facbac1e5f6af5387d6e79b1fe1559fa45
ddrdi459-png <ddrdi459@gmail.com>
2026-08-18 16:50:03 +0300

    Ajouter la configuration email interne Admin pour l'identifiant public

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,4 @@
 window.TSARANORO_SUPABASE_URL = 'https://ksdhdceasgubtixyzqdd.supabase.co';
 window.TSARANORO_SUPABASE_KEY = 'sb_publishable_i_lzT1JrzRG2iFmnTtU9pQ_EJKCzXe5';
 window.TSARANORO_ADMIN_UID = '1717cd49-05c9-4321-8007-d4d317ddc630';
+window.TSARANORO_ADMIN_EMAIL = 'tsaranoro.admin@gmail.com';
